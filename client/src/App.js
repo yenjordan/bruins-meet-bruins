01d34709f9ce7b395c46e4adc9348adb92c2352c
Larry <larrylim13542@gmail.com>
2024-11-06 22:58:11 -0800

    Initial login page test

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import Landing from './pages/Landing'
+import {BrowserRouter, Routes, Route} from 'react-router-dom' 
+//installed routing with npm i react-router-dom@6, other group members just need to run 'npm install'
+
+
+//routing is what the links will look like, ex: chat page will be localhost.../chat,
+//home page woudl just be localhost.../
+const App = () => {
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path={"/"} element={<Landing/>}/>
+      
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,24 @@
+//home page, created separate folder pages to have all 5 files in it
+//also these files has to be capitalize
+
+const Landing = () => {
+    const handleClicks = () => {    //log clicks 
+        console.log('click')
+    }
+    
+
+    return(
+        <>
+        <div className="landing">
+            <h1>Bruin Meets Bruin</h1>
+            <h2>Come Meet Other Bruins!</h2>
+            <button className="mainbutton" onClick={handleClicks}>
+                {'Sign in'}
+            </button>
+            
+        </div>
+        </>
+    )
+}
+
+export default Landing
