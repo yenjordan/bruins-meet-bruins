27225fede624f65808d66d3a97619ec179a8d697
mat319 <831cheesehead@gmail.com>
2024-11-11 23:15:26 -0800

    Fixed minor errors and replaced Location with Last name

--- client/src/pages/HomePage/CreateProfile.js
+++ client/src/pages/HomePage/CreateProfile.js
@@ -1,27 +1,37 @@
 import {useState} from 'react';
+import './CreateProfile.css'
 
 export default function Profile() {
-    const[name, setName] = useState('');
+    const[fName, setfName] = useState('');
+    const[lName, setlName] = useState('');
     const[age, setAge] = useState('');
-    const[location, setLocation] = useState('');
     const[aboutMe, setAboutMe] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(name);
+        console.log(fName);
     }
 
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label for="name">Name</label>
-            <input value={name} onChange={(e) => setName(e.target.value)} placeholder='Joe' id='name' />
-            <label for="age">Age</label>
-            <input value={age} onChange={(e) => setAge(e.target.value)} placeholder='Bruin' id='age' />
-            <label for="location">Location</label>
-            <input value={location} onChange={(e) => setLocation(e.target.value)} placeholder='UCLA' id='location' />
-            <label for="aboutMe">About Me</label>
-            <input value={aboutMe} onChange={(e) => setAboutMe(e.target.value)} placeholder='About Me' id='aboutMe' />
+        <form onSubmit={handleSubmit} className='form-container'>
+            <h1>Create your Profile</h1>
+            <div className='form-field'>
+                <label for="name">First Name</label>
+                <input value={fName} onChange={(e) => setfName(e.target.value)} placeholder='Joe' id='firstName' />
+            </div>
+            <div className='form-field'>
+                <label for="lName">Last Name</label>
+                <input value={lName} onChange={(e) => setlName(e.target.value)} placeholder='Bruin' id='lastName' />
+            </div>
+            <div className='form-field'>
+                <label for="age">Age</label>
+                <input value={age} onChange={(e) => setAge(e.target.value)} placeholder='18' id='age' />
+            </div>
+            <div className='form-field'>
+                <label for="aboutMe">About Me</label>
+                <input value={aboutMe} onChange={(e) => setAboutMe(e.target.value)} placeholder='About Me' id='aboutMe' />
+            </div>
             <button type="submit">Create Profile</button>
         </form>
     )
