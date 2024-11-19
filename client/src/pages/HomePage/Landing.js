b80580e8fe2db47393c91c8c271d6f22ef7ff2bf
yenjordan <yenjordan@gmail.com>
2024-11-19 00:06:21 -0800

    implemented bare-bones search page

--- client/src/pages/HomePage/Landing.js
+++ client/src/pages/HomePage/Landing.js
@@ -1,36 +1,36 @@
 //home page, created separate folder pages to have all 5 files in it
 //also these files has to be capitalize
 
-import './Landing.css'
-import { useState } from 'react'
-import AuthUi from '../../components/AuthUi'
+import './Landing.css';
+import { useState } from 'react';
+import AuthUi from '../../components/AuthUi';
 
 const Landing = () => {
-    const logToken = false
-    const [showAuth, setAuth] = useState(false)     //flag to show the authentiation interface
-    const [hasAccount, setAccount] = useState(false) //flag to determine whether UI should show "log in" or "create acc"
+    const logToken = false;
+    const [showAuth, setAuth] = useState(false); // Flag to show the authentication interface
+    const [hasAccount, setAccount] = useState(false); // Flag to determine whether UI should show "log in" or "create acc"
 
-    const handleClicks = (loginflag) => {    //log clicks, pass in parameter that will either be true or false
-        console.log('click')
-        setAuth(true)
-        setAccount(loginflag)    //true = create account, false = login  
-    }
+    const handleClicks = (loginflag) => { // Log clicks, pass in parameter that will either be true or false
+        console.log('click');
+        setAuth(true);
+        setAccount(loginflag); // true = create account, false = login
+    };
 
-    return(
+    return (
         <>
-        <div className="landing">
-            <h1>Bruin Meets Bruin</h1>
-            <h2>Come Meet Other Bruins!</h2>
-            <button className="mainButton" onClick={() => handleClicks(true)}>  
-                {logToken ? 'Signout' : 'Create an Account'}      
-            </button>
-            <button className="mainButton" onClick={() => handleClicks(false)} >
-                {"Log in"}
-            </button>
-            {showAuth && (<AuthUi setAuth={setAuth} hasAccount={hasAccount} setAccount={setAccount}/>)}
-        </div>
+            <div className="landing">
+                <h1>Bruin Meets Bruin</h1>
+                <h2>Come Meet Other Bruins!</h2>
+                <button className="mainButton" onClick={() => handleClicks(true)}>
+                    {logToken ? 'Signout' : 'Create an Account'}
+                </button>
+                <button className="mainButton" onClick={() => handleClicks(false)}>
+                    {"Log in"}
+                </button>
+                {showAuth && (<AuthUi setAuth={setAuth} hasAccount={hasAccount} setAccount={setAccount} />)}
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default Landing
+export default Landing;
