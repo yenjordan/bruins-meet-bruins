2185c7bdb563c50a6442fdc5d32cf1b253cf455b
mat319 <831cheesehead@gmail.com>
2024-11-25 20:06:36 -0800

    Created profile model/schema.

---
+++ server/model/profile.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const profile = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    age: {
+        type: Number,
+        required: true
+    },
+
+    bio: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("profile", profile)
