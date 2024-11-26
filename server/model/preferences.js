8a88dc16648f18514edac693ff33d9ad32be8036
Larry <larrylim13542@gmail.com>
2024-11-25 17:09:28 -0800

    slightly modifed preferences schema to align with matching implementation

--- server/model/preferences.js
+++ server/model/preferences.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose")
 //model for preferences: age range, hobbies
 const MyPreferences = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
     ageRange: {
-        min: { type: Number, required: true },
-        max: { type: Number, required: true },
+        min: { type: Number, required: true, default: 0},
+        max: { type: Number, required: true, default: -1},
     },
     hobbyPreferences: {
         type: [String],
         required: true,
+        default: []
     }
 
 }, { collection: 'preferences' });
