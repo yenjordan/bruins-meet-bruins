41a506464f9717ea0731b42dedc8ec94c8b8d295
mat319 <831cheesehead@gmail.com>
2024-11-27 04:01:11 -0800

    Added protection to the route with auth

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const { signup, login } = require('../controllers/authControll')
 const Profile = require('../model/profile')
+const authenticate = require('../middleware/auth')
 
 const route = express.Router()
 route.post('/signup', signup)
 route.post('/login', login)
 
-route.post('/createProfile', async(req, res) => {
+route.post('/createProfile', authenticate, async(req, res) => {
     const{firstName, lastName, age, bio} = req.body;
 
     if(!firstName || !lastName || !age || !bio){
@@ -17,7 +18,8 @@ route.post('/createProfile', async(req, res) => {
             firstName,
             lastName,
             age,
-            bio
+            bio,
+            userID: req.userID
         });
 
         await newProfile.save();
