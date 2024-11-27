1d53a9a9efb7a30d98dd658c27f2bbf620d07418
mat319 <831cheesehead@gmail.com>
2024-11-27 03:59:31 -0800

    Created middlewares that verifies JWT

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(403).json({ message: 'No token provided' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId; // Attach userId to the request object
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+};
+module.exports = authenticate;
